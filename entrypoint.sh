01b7324844dc8a8d1a310f914528ec3b23e744d8
balu100 <ebalu1@hotmail.com>
2021-08-31 10:13:00 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 if [ -e /opt/installed ]
 then
 echo "Installed"
-smbd &
-wsdd &
+smbd
+wsdd
 echo done
 else
 echo "Install"
@@ -12,12 +12,16 @@ apt -y upgrade
 apt install -y wget
 apt install -y gnupg2
 wget -qO - https://pkg.ltec.ch/public/conf/ltec-ag.gpg.key | apt-key add -
-echo "deb https://pkg.ltec.ch/public/ focal main" | sh -c 'cat >> /etc/apt/sources.list'
+rm /etc/apt/sources.list.d/wsdd.list
+echo "deb https://pkg.ltec.ch/public/ focal main" | sh -c 'cat > /etc/apt/sources.list.d/wsdd.list'
 apt update
 apt install -y wsdd
+mv /etc/samba/smb.conf /etc/samba/smb.conf.bak
 apt install -y samba
-smbd &
-wsdd &
+mv /etc/samba/smb.conf.bak /etc/samba/smb.conf
+touch /opt/installed
+smbd
+wsdd
 echo done
 touch /opt/installed
 fi
